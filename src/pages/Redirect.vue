14f5bdb2a0dcd00db140056cbd14ec6d823139d3
Edward <you@example.com>
2020-03-14 22:30:54 +0800

    Fix Redirect

---
+++ src/pages/Redirect.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <FormMain v-if="!tree.submitted" :tree="tree" />
+    <TreeMain v-else :tree="tree" />
+  </q-page>
+</template>
+
+<script>
+export default 
+{
+  name: 'RedirectPage',
+    created()
+    {
+        return this.$router.push('/index');
+    }
+}
+</script>
